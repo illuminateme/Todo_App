0f4726bf373dd052c073a502ad28bf525bda0131
illuminateme <aimaatigari@gmail.com>
2021-06-23 14:17:58 +0100

    23/no2

--- app/src/main/java/com/aima/todoapp/activities/ProfileActivity.kt
+++ app/src/main/java/com/aima/todoapp/activities/ProfileActivity.kt
@@ -1,12 +1,27 @@
 package com.aima.todoapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.aima.todoapp.R
+import com.aima.todoapp.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    private lateinit var homeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+
+     // same function as above   homeButton = findViewById(R.id.homeButton)
+        homeButton.setOnClickListener {
+            val anyIntent = Intent(this,HomeActivity::class.java)
+            startActivity(anyIntent)
+        }
     }
 }
